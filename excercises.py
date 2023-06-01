c60e3af913e46dd0a22b40bacb0cba9992a0424a
JacobEsc2004 <utm22030618@utma.edu.mx>
2023-05-31 22:17:25 -0600

    Feat: A dictionary is added and it asks for a correction

--- excercises.py
+++ excercises.py
@@ -3,8 +3,13 @@ for i in range(1,6):
     print("Add a new item to the list")
     list[i-1] = input()
     print ("New element: ", list[i-1])
- pets = ('Manchitas', 'Fiona', 'Betoben', 'Morgan', 'Luno', 'Solo', 'Mimbi')
+pets = ('Manchitas', 'Fiona', 'Betoben', 'Morgan', 'Luno', 'Solo', 'Mimbi')
 j = 1
 while j<8:
     print(pets[j-1])
     j=j+1
+verbs = {"Jump":"Saltar", "Run":"Correr", "Walk":"Volar"}
+print (verbs)
+print("Write the correct verb in spanish for 'Walk'")
+verbs['Walk']= input()
+print (verbs)
