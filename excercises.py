ac436f06a3324f94c79027045b23efa17364da61
JacobEsc2004 <utm22030618@utma.edu.mx>
2023-05-31 21:59:07 -0600

    Feat: A tuple with seven elements is added and printed with a while loop

--- excercises.py
+++ excercises.py
@@ -3,4 +3,8 @@ for i in range(1,6):
     print("Add a new item to the list")
     list[i-1] = input()
     print ("New element: ", list[i-1])
-
+ pets = ('Manchitas', 'Fiona', 'Betoben', 'Morgan', 'Luno', 'Solo', 'Mimbi')
+j = 1
+while j<8:
+    print(pets[j-1])
+    j=j+1
